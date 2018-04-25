b602e70230c3d2f03d60a673c59aad8d9fa4f2d7
yevgeniy <chernovzhenia@gmail.com>
2018-04-25 16:08:26 +0300

    added sitecontroller

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,12 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SiteController@index');
 
 Auth::routes();
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
-    Route::get('/', 'AdminController@index')->name('admin');
+    Route::get('/', 'AdminController@index');
     Route::resource('user', 'UserController');
     Route::resource('visitors', 'VisitorsController');
 });
